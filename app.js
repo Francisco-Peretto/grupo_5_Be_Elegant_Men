a440ed8a4d8d025a1a2782cbcfc372f9e0ad50e1
jamaneffa <jamaneffa@yahoo.com>
2022-07-26 19:37:24 -0300

    creacion de formularios

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ const port = 3030;
 app.listen(port, () => console.log("Servidor en linea"))
 
 app.get('/', (req , res) => res.sendFile(path.resolve(__dirname,'./views/index.html')))
+app.get('/login', (req , res) => res.sendFile(path.resolve(__dirname,'./views/login.html')))
+app.get('/register', (req , res) => res.sendFile(path.resolve(__dirname,'./views/register.html')))
 
 app.use(express.static(publicPath))
