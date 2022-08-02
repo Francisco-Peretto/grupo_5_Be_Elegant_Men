34c041c567c2c476f06e64be2cbfeaac7ef291fb
jamaneffa <jamaneffa@yahoo.com>
2022-08-02 17:36:33 -0300

    cambio de estructura de archivos

---
+++ source/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+const publicPath = path.resolve(__dirname,'../public');
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => console.log("Servidor en linea"))
+
+app.get('/', (req , res) => res.sendFile(path.resolve(__dirname,'views/index.html')))
+app.get('/login', (req , res) => res.sendFile(path.resolve(__dirname,'views/login.html')))
+app.get('/register', (req , res) => res.sendFile(path.resolve(__dirname,'views/register.html')))
+app.get('/productDetail', (req , res) => res.sendFile(path.resolve(__dirname,'views/productDetail.html')))
+app.get('/cart', (req , res) => res.sendFile(path.resolve(__dirname,'views/cart.html')))
+
+app.use(express.static(publicPath));
