4b6012ac0a6699908cda44580e81a3d2bf55ed82
Kangilesh <f.peretto.89@gmail.com>
2022-09-22 21:47:56 -0300

    Migración a SQL, creación de data.sql

--- source/data/structure.sql
+++ source/data/structure.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bemen_db`
+-- Database: `beelegantmen_db`
 --
 
 -- --------------------------------------------------------
