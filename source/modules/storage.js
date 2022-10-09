3a59853c3d58a65080db152d83b780d4ccc27cb6
Kangilesh <f.peretto.89@gmail.com>
2022-10-09 20:32:27 -0300

    Arreglo de loops en editProduct y cambio en el almacenamiento de productos con Multer

--- source/modules/storage.js
+++ source/modules/storage.js
@@ -4,7 +4,8 @@ const {diskStorage} = require('multer');
 
 let destination = folder => (req, file, callback) =>{
     let path = "";
-    req.body.email ? path = resolve(__dirname,'..','..','public','img','users') : path = resolve(__dirname,'..','..','public','img','products',req.body.category);
+    let category = req.body.category == "Ambos" ? "1" : req.body.category == "Camisas" ? "2" : req.body.category == "Corbatas" ? "3" : req.body.category == "Pantalones" ? "4" : req.body.category == "Sacos" ? "5" : "6";
+    req.body.email ? path = resolve(__dirname,'..','..','public','img','users') : path = resolve(__dirname,'..','..','public','img','products', category);
     if(!fs.existsSync(path)){
         fs.mkdirSync(path)
     }
