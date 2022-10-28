376593d0efc6556066ef2d1b3722545998ae481b
jamaneffa <jamaneffa@yahoo.com>
2022-10-28 12:23:14 -0300

    create changeAvatar script

--- public/scripts/changeAvatar.js
+++ public/scripts/changeAvatar.js
@@ -1,4 +1,20 @@
+/* OPCION 1 */
+
 document.getElementById('avatar').addEventListener('click', function () {
-    document.getElementById('avatarID').src = document.getElementById('avatar').value;
+    document.getElementById('avatarImgID').src = document.getElementById('avatar').value;
+});
+
+/* OPCION 2 */
+
+const inputFile = document.querySelector('#avatar')
+const imgTag = document.querySelector('#avatarImgID')
+
+inputFile.addEventListener('change', () => {
+
+  const archivos = inputFile.files;
+  const primerArchivo = archivos[0]
+  const objectURL = URL.createObjectURL(primerArchivo);
+
+  imgTag.src = objectURL;
 });
 
