0ff4f7d644ee774e58bb66928ee0d06d23f91d8e
Kangilesh <f.peretto.89@gmail.com>
2022-11-19 17:37:21 -0300

    Cambios visuales en Dashboard

--- dashboard/src/components/ChartRow.js
+++ dashboard/src/components/ChartRow.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function ChartRow(props){
+    let category = props.relaciones.categoria;
+    let categoryCap = category.charAt(0).toUpperCase() + category.slice(1);
     return (
                 <tr>
                     <td>{props.sku}</td>
                     <td>{props.name}</td>
                     <td>${props.price}</td>
-                    <td>{props.relaciones.categoria}</td>
+                    <td>{categoryCap}</td>
                 </tr>
             )
     }
