71920ffb93781d2a6f4e168b3764a212a19c181f
jamaneffa <jamaneffa@yahoo.com>
2022-11-23 13:50:03 -0300

    change bars menu

---
+++ website/public/scripts/barsMenu.js
@@ -0,0 +1,9 @@
+var menu = document.querySelector('.hamburger');
+
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+menu.addEventListener('click', toggleMenu, false);
