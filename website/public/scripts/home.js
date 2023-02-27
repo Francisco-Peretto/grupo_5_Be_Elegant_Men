4de3e87453e48e7f56d930988e42fc5d37be7641
Kangilesh <f.peretto.89@gmail.com>
2023-02-27 19:11:25 -0300

    Cambios en el listado de productos de searchbar

--- website/public/scripts/home.js
+++ website/public/scripts/home.js
@@ -1,16 +1,43 @@
-document.querySelector("#search").addEventListener("input", async (e) => {
-    try {
-        let petition = await fetch ('http://localhost:3030/api/products/');
-        let response = await petition.json();
-        let products = response.product;
-        let names = products.map(product => product.name)
+const format = text => String(text).toLowerCase().trim();
 
-        let filtro = name => String(name).toLowerCase().trim().includes(String(e.target.value).toLowerCase().trim());
-        let resultados = e.target.value.length > 1 ? names.filter(filtro) : [];
-        
-        document.querySelector("#suggestions").innerHTML = null;
-        resultados.forEach(element => document.querySelector("#suggestions").innerHTML += `<option value="${element}">${element}</option>`);
-    } catch (error) {
-        console.log(error);
+window.addEventListener('load', async (e) => {
+    try {
+        let petition = await fetch('http://localhost:3030/api/products/');
+        let response = await petition.json();
+        localStorage.setItem('products', JSON.stringify(response));
+        localStorage.setItem('productNames', JSON.stringify(response.product.map(product => ({name: format(product.name), sku: product.sku}))));
+
+        document.querySelector("#search").addEventListener("input", async (event) => {
+            try {
+                const products = JSON.parse(localStorage.getItem('products')).product;
+                let filtro = ({name, sku}) => format(name).includes(format(event.target.value));
+
+                let resultados = event.target.value.length > 1 ? JSON.parse(localStorage.getItem('productNames'))
+                .filter(filtro) : [];
+
+                console.log(JSON.parse(localStorage.getItem('products')));
+                document.querySelector("#suggestions").innerHTML = null;
+                console.log(resultados);
+
+                resultados.forEach(element => {
+                    const link = document.createElement('a');
+                    link.href = `/products/${element.sku}`;
+                    link.textContent = element.name;
+
+                    link.addEventListener('click', (event) => {
+                        event.preventDefault();
+                        window.location.href = link.href;
+                    });
+                    const option = document.createElement('option');
+                    option.appendChild(link);
+                    document.querySelector("#suggestions").appendChild(option);
+                });
+
+            } catch (error) {
+        console.log(error.message);
     }
-})
+    });
+} catch (error) {
+    console.log(error.message);
+}
+});
